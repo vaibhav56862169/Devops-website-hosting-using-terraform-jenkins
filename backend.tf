d138c8790a031cf5e3cd9bce3a2661d570043a51
vaibhav56862169 <78777319+vaibhav56862169@users.noreply.github.com>
2024-04-11 18:00:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "jenkinsbucket777"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "dynamo-db-table"
   }
 }
