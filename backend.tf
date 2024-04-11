496c2c26a847a071ecad2408b0c5b91734b9cdc3
vaibhav56862169 <78777319+vaibhav56862169@users.noreply.github.com>
2024-04-11 18:02:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "jenkinsbucket777"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "dynamo-db-table"
   }
 }
