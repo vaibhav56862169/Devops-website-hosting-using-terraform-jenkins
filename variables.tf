2ec0118dd7283e975de6dfa532aa7ffc96b4f6b0
vaibhav56862169 <78777319+vaibhav56862169@users.noreply.github.com>
2024-04-11 23:53:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "mybucket"
+  default     = "jenkinsbucket777"
 }
 
 variable "acl" {
